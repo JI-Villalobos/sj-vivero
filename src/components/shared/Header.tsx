e4ca873e473de04f415e0329712d9b2517307002
JI-Villalobos <chuy12villalobos@gmail.com>
2024-07-24 12:49:40 -0600

    add header component

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image"
+import Link from "next/link"
+import { BiSolidSend } from "react-icons/bi"
 
 export const Header = () => {
     return (
-        <div className="">
-            <header>
-                <Image src="/sj-vivero.png" width={80} height={80} alt="Company logo"/>
+        <div className="w-full">
+            <header className="flex flex-row justify-between w-full items-center bg-mp-gray-soft">
+                <Image src="/sj-vivero.png" width={120} height={120} alt="Company logo" className="p-4"/>
+                <Link href="/login" className="p-4">
+                    <BiSolidSend size={20}/>
+                </Link>
             </header>
         </div>
     )
