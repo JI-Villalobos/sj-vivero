76c41c6324eaeb7e596ed435b46e39c66174bb8e
JI-Villalobos <chuy12villalobos@gmail.com>
2024-08-22 13:29:02 -0600

    update table styles

--- src/ui/user-home/RowItem.tsx
+++ src/ui/user-home/RowItem.tsx
@@ -14,9 +14,9 @@ export const RowItem = ({ account }: Props) => {
             <td className="py-2 px-6 text-sm text-mp-dark">{formatDate(account.date)}</td>  
             <td className="py-2 px-6 text-sm text-mp-blue">{formatAmount(summarize(account.incomeRegistries))}</td>
             <td className="py-2 px-6 text-sm text-mp-green">{formatAmount(summarize(account.expenseRegistries))}</td>
-            <td>
-                <Link href="#" className="rounded p-2 flex items-center justify-center">
-                    <BiSolidShow color="gray" size={30}/>
+            <td className="py-2 px-6 text-sm text-mp-dark   ">
+                <Link href="#" className="rounded flex items-center justify-center">
+                    <BiSolidShow size={20}/>
                 </Link>
             </td>
         </tr>
